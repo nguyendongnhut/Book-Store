5ce6f61f2a0a1218512272b70abb263a6f201150
nhut <nguyendongnhutqn1996@gmail.com>
2020-10-07 00:58:27 +0700

    update code delete order

--- src/Pages/AllOrder/AllOrder.jsx
+++ src/Pages/AllOrder/AllOrder.jsx
@@ -33,10 +33,46 @@ function AllOrder(props) {
     }
 
     FetchAllOrders();
-  }, []);
+  }, [orders.length]);
 
-  const handleDeleteOrder = () => {
-    console.log("asfsdf");
+  const deleteOrderDetail = async (orderId) => {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    };
+
+    const response = await fetch(
+      `http://localhost:3001/api/cart/deleteOrderDetail/orderId/${orderId}`,
+      requestOptions
+    );
+  };
+
+  const handleDeleteOrder = async (orderId) => {
+    deleteOrderDetail(orderId);
+
+    let deleteOrder = orders.filter((item) => {
+      return item.orderId === orderId;
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    };
+
+    const response = await fetch(
+      `http://localhost:3001/api/cart/deleteOrder/orderId/${orderId}`,
+      requestOptions
+    );
+
+    setOrders(deleteOrder);
   };
 
   return (
